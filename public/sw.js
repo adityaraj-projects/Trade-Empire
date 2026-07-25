8eeb600479109e03ade4451a33fbf5e111757235
adityaraj-projects <personalid1105@gmail.com>
2026-07-25 15:35:54 +0530

    feat: complete Android Chrome and iPhone Safari PWA installability (Add to Home Screen)

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,36 @@
-const CACHE_NAME = 'trade-empire-v1';
+const CACHE_NAME = 'trade-empire-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
-  '/favicon.ico',
+  '/favicon.svg',
+  '/manifest.json',
 ];
 
-self.addEventListener('install', (event: any) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event: any) => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
